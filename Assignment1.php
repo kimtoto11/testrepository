f5f500c4e7859f897889a9e88523dac0aa6673ba
Student <norknork11@gmail.com>
2021-09-11 20:10:48 -0700

    Here are my files

---
+++ Assignment1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Test File</title>
+</head>
+<body>
+ <p>Kim Cyrus N. Cervantes, The N stands for Nagales.</p>
+ <p>Student ID number:920906529</p>
+<?php
+date_default_timezone_set('America/Los_Angeles');
+$stamp3 = date("m-d-Y h:i:s");
+print $stamp3;
+?>
+<p><img src="me.jpg" width=200></p>
+</body>
+</html
